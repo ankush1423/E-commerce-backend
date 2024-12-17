509d795e408a6071b864a3f516e3e17970886fde
ankush1423 <139746905+ankush1423@users.noreply.github.com>
2024-12-17 09:55:08 +0530

    working on product route

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -36,3 +36,67 @@ export const createReview = asyncHandler(async(req,res) => {
 
 })
 
+export const updateReview = asyncHandler(async(req,res) => {
+       const {reviewId} = req.params
+       const {comment , rating} = req.body
+
+       if(!reviewId || !comment || !rating)
+       {
+           throw new ApiError(400,"error while getting the details")
+       }
+
+       const updatedReview = await Review.findByIdAndUpdate(
+             reviewId,
+             {
+                $set : {
+                   comment : comment,
+                   rating : rating
+                }
+             },
+             {
+                new : true
+             }
+       )
+
+       if(!updatedReview)
+       {
+          throw new ApiError(400,"error while updating the review")
+       }
+
+       return res
+              .status(200)
+              .json(
+                  new ApiResponse(
+                      updatedReview,
+                      "review Updated successfully"
+                  )
+              )
+})
+
+export const deleteReview = asyncHandler(async(req,res) => {
+      const {reviewId} = req.params
+
+      if(!reviewId)
+      {
+         throw new ApiError(400,"error while geting the reviewID")
+      }
+
+      const deletedReview = await Review.findByIdAndDelete(reviewId)
+
+      if(!deletedReview)
+      {
+          throw new ApiError(400,"error while deleting the Review")
+      }
+      
+      return res
+             .status(200)
+             .json(
+                new ApiResponse(
+                     {},
+                     "review deleted successfully"
+                )
+             )
+
+})
+
+
--- src/routes/review.route.js
+++ src/routes/review.route.js
@@ -1,10 +1,16 @@
 import express from "express"
 import {
        createReview,
+       updateReview,
+       deleteReview
       } from "../controllers/review.controller.js"
+import {authUser} from "../middlewares/auth.middleware.js"
+
 
 const router = express.Router()
 
-router.route("/:productId").post(createReview)
+router.use(authUser)
 
+router.route("/:productId").post(createReview).patch(updateReview).delete(deleteReview)
+ 
 export default router
